e54ff024252a93012285d8405c26269e64b1e06c
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-01-06 18:08:43 -0600

    Exercises for Conditionals and Debugging.

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -1,6 +1,7 @@
 let engineIndicatorLight = 'red blinking';
 let fuelLevel = 21000;
 let engineTemperature = 1200;
+let commandOverride = true;
 
 /* 5) Implement the following checks using if/else if/else statements:
 
@@ -22,3 +23,42 @@ f) Otherwise, print "Fuel and engine status pending..." */
 
 /* 6) b) Code the following if/else check:
 If fuelLevel is above 20000 AND engineIndicatorLight is NOT red blinking OR commandOverride is true print "Cleared to launch!" Else print "Launch scrubbed!" */
+if(fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "Red Blinking")
+{
+   console.log("ENGINE FAILURE IMMINENT!");
+}
+else if(fuelLevel <= 5000 || engineTemperature > 2500 )
+{
+   console.log("Check fuel level. Engines running hot.");
+}
+else if(fuelLevel > 20000 && engineTemperature <= 2500)
+{
+   console.log("Full tank. Engines good.");
+}
+else if(fuelLevel > 10000 && engineTemperature <=2500)
+{
+   console.log("Fuel level above 50%. Engines good.");
+}
+else if(fuelLevel > 5000 || engineTemperature <=2500)
+{
+   console.log("Fuel level above 25%. Engines good.");
+}
+else
+{
+   console.log("Fuel and engine status pending…");
+}
+
+if(commandOverride==false)
+{ 
+    if( ((fuelLevel > 20000) && (engineIndicatorLight != 'red blinking' ))||commandOverride==true) {
+      console.log("Cleared to Launch!");
+   } 
+    else {
+      console.log("Launch scrubbed");
+   }
+}
+
+    else 
+    {
+      console.log("Cleared to Launch!");
+    }
