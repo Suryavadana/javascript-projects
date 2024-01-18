d4e68a2c7066886d5cc8443ee8ed2aa6139c521b
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-01-18 00:11:56 -0600

    Strings Exercises.

--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,8 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "lions";
+let name = 'leo' ;
+let verb = 'play';
+let adjective = 'good';
+let color = 'white';
 
 console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+console.log(`JavaScript provides a ${color}  collection of tools — including ${adjective} syntax and ${pluralNoun}  — that allows ${name} to ${verb} with strings.`);
--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -1,11 +1,18 @@
 //String methods can be combined in a process called method chaining.
 
-let word = 'JavaScript';
+let word = 'Java Script';
+let trimmed = word.trim();
+let subs = "Script";
 
 console.log(word.toUpperCase());
 //Returns ``JAVASCRIPT``
 
 //What does ``word.slice(4).toUpperCase()`` return?
-
+console.log(word.slice(4).toUpperCase());
 
 //Experiment with other combinations (chains) of string methods.
+console.log(word.indexOf(subs));
+console.log(word.toLowerCase());
+console.log(trimmed);
+console.log(word.replace('J', 'Q'));
+console.log(word.slice(1, 6));
