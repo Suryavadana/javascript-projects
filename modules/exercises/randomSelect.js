c86d881d505efe4f7f0875afdac1e51d0d63c093
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-02-04 23:29:51 -0600

    modules exercises

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,10 @@
 function randomFromArray(arr){
   //Your code here to select a random element from the array passed to the function.
+  arr = arr[Math.floor(Math.random()*arr.length)];
+  return arr;
 }
 
 //TODO: Export the randomFromArray function.
+module.exports = {
+  randomFromArray: randomFromArray
+};
