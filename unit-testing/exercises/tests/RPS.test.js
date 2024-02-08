dece222b3c49321e14c0ac6a4418d0795d123e51
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-02-08 14:06:34 -0600

    unit-testing exercises

--- unit-testing/exercises/tests/RPS.test.js
+++ unit-testing/exercises/tests/RPS.test.js
@@ -1,16 +1,18 @@
-const RPS = require('../RPS.js');
+const whoWon = require('../RPS.js');
+
+describe("whoWon", function(){
 test("returns 'Player 2 wins!' if P1 = rock & P2 = paper", function(){
-    let output = test.whoWon('rock','paper');
+    let output = whoWon.whoWon('rock','paper');
     expect(output).toBe("Player 2 wins!");
  });
  
  test("returns 'Player 2 wins!' if P1 = paper & P2 = scissors", function(){
-    let output = test.whoWon('paper','scissors');
+    let output = whoWon.whoWon('paper','scissors');
     expect(output).toBe("Player 2 wins!");
  });
 
- test("returns 'Player 2 wins!' if P1 = paper & P2 = scissors", function(){
-   let output = test.whoWon('scissors','rock');
+ test("returns 'Player 2 wins!' if P1 = scissors & P2 = rock", function(){
+   let output = whoWon.whoWon('scissors','rock');
    expect(output).toBe("Player 2 wins!");
 });
- 
+});
--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -10,6 +10,7 @@ describe("checkFive", function(){
    });
       test("returns 'num is less than 5' when num < 5.", function() {
    // test code //
+   let output = checkFive(2);
    expect(output).toBe("2 is less than 5.");
 });
    test("returns 'num is greater than 5' when num > 5.", function() {
@@ -20,6 +21,6 @@ describe("checkFive", function(){
    test("returns 'num is equal to 5' when num = 5.", function() {
       // test code //
       let output =checkFive(5);
-      expect(output).toBe("5 is equal to 5.");
+      expect(output).toEqual("5 is equal to 5.");
    });
 });
