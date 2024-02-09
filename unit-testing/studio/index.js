bf19f743b0063fbf9e17335abbc02edc07a5cae3
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-02-08 21:05:20 -0600

    unit-testing Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,51 @@
 
 let launchcode = {
-  
+    Organization : "non profit",
+    executiveDirector : "Jeff",
+   percentageCoolEmployees : 100,
+    programsOffered : ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput(number){
+        let result ='';
+        if(number%2===0 && number%3===0 && number%5===0)
+        {
+            result +="LaunchCode Rocks!";
+        }
+        else if(number%2===0 && number%3===0)
+        {
+            result += "LaunchCode!";
+        }
+        else if(number%3===0 && number%5===0)
+        {
+            result+= "Code Rocks!";
+        }
+        else if(number%2===0 && number%5===0)
+        {
+            //result+= ("Launch Rocks!" );
+            //if(result == 'Launch Rocks!')
+                result+= "Launch Rocks! (CRASH!!!)";
+
+        }        
+        else if(number%2===0)
+        {
+            result+= "Launch!";
+        }
+        else if(number%3===0)
+        {
+            result+= "Code!";
+        }
+        else if(number%5===0)
+        {
+            result+= "Rocks!";
+        }
+        else if(number%2!=0 && number%3!=0 && number%5!=0)
+        {
+            return "Rutabagas! That doesn't work.";
+        }
+        return result;
+
+    }
 }
+console.log(launchcode.launchOutput(11));
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,49 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+
+  test("return value object key matches values ",()=>{
+    expect(launchcode.Organization).toBe('non profit');
+    expect(launchcode.executiveDirector).toBe('Jeff');
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+
+  });
+  test("list of program includes Web Development, Data Analysis, Liftoff",()=>{
+    expect(launchcode. programsOffered.includes("Web Development")).toBe(true); 
+   expect(launchcode. programsOffered.includes( "Data Analysis")).toBe(true);
+   expect(launchcode. programsOffered.includes("Liftoff")).toBe(true);
+   expect(launchcode.programsOffered.length).toBe(3);
+  });
+
+  test("number divisble ONLY by 2",function(){
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  });
+  test("number divisble ONLY by 3",function(){
+    expect(launchcode.launchOutput(3)).toBe("Code!");
+  });
+  test("number divisble ONLY by 5",function(){
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+  });
+  test("number divisble by 2 and 3",function(){
+    expect(launchcode.launchOutput(6)).toBe( "LaunchCode!");
+  });
+  test("number divisble by 3 and 5",function(){
+    expect(launchcode.launchOutput(15)).toBe( "Code Rocks!");
+  });
+  test("number divisble by 2 and 5",function(){
+   // expect(launchcode.launchOutput(10)).toBe("Launch Rocks!");
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!)");
+    
+  });
+  test("number divisble by 2 ,3 and 5",function(){
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+  });
+
+  test("number NOT divisble by 2 ,3 and 5",function(){
+    expect(launchcode.launchOutput(11)).toBe("Rutabagas! That doesn't work.");
+  });
+ 
+
+  });
+
   
-});
