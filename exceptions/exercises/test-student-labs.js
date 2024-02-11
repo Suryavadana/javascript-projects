7a09292fe2c151a1b11476be7709c45a5b597b2f
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-02-11 14:39:12 -0600

    exceptions exercises

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,9 +1,16 @@
 function gradeLabs(labs) {
+  try{
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
+}
+catch(err){
+  result = "Error thrown";
+  console.log("Error thrown");
+}
+
 }
 
 let studentLabs = [
@@ -21,4 +28,25 @@ let studentLabs = [
   }
 ];
 
-gradeLabs(studentLabs);
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
