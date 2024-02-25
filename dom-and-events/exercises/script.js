e0a28296daaa47cd62de7a9b2edf37dae6d99553
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-02-25 13:19:56 -0600

    Dom-and-events Exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,22 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+    abortMission.addEventListener("click", function( event ) {
+        event.target.style.backgroundColor = "red";
+        if(window.confirm("Are you sure you want to abort the mission?")) {
+            paragraph.outerHTML='Mission aborted! Space shuttle returning home';
+          }
+     });
+    
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+
     
 }
 
 window.addEventListener("load", init);
+
