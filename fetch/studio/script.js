12aabb15894b5e3934899582bc0071aef7474d9a
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-03-03 23:56:49 -0600

    Studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,28 @@
 //TODO: Add Your Code Below
+window.addEventListener("load",function(){
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+  .then(response => response.json())
+  .then(function(data){
+    let container = document.getElementById("container");
+    let astronauts=[];
+    for(let i=0;i<data.length;i++)
+    {
+      let astronaut=data[i];
+      const astronautDetails =[
+      "<div class = 'astronaut'>",
+        "<div class = 'bio'>",
+          "<h3>"+astronaut.firstName +" "+ astronaut.lastName+"</h3>",
+          "<ul>",
+            "<li>Hours in space: " + astronaut.hoursInSpace+"</li>",
+            "<li>Active: "+ (astronaut.active ?  'Yes' : 'No') +"</li>",
+            "<li>Skills: " + astronaut.skills.join(', ')+"</li>",
+          "</ul>",
+        "</div>",
+        "<img class='avatar' src='" + astronaut.picture + "' alt='" + astronaut.firstName + " " + astronaut.lastName + "'></img>",
+      "</div>"
+      ];
+      astronauts.push(astronautDetails.join(''));
+    }
+    container.innerHTML=astronauts.join('');
+  });
+});
